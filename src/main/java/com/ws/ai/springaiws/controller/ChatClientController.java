a4df9dcbe4032a014eb22e3026c7cdb0f9f9efe1
e-shun.wang3 <e-shun.wang3@zeekrlife.com>
2024-12-10 15:55:00 +0800

    text2Speech

--- src/main/java/com/ws/ai/springaiws/controller/ChatClientController.java
+++ src/main/java/com/ws/ai/springaiws/controller/ChatClientController.java
@@ -7,16 +7,19 @@ import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.image.ImagePrompt;
 import org.springframework.ai.image.ImageResponse;
-import org.springframework.ai.openai.OpenAiChatOptions;
-import org.springframework.ai.openai.OpenAiImageModel;
-import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.ai.openai.*;
+import org.springframework.ai.openai.api.OpenAiAudioApi;
 import org.springframework.ai.openai.api.OpenAiImageApi;
+import org.springframework.ai.openai.audio.speech.SpeechPrompt;
+import org.springframework.ai.openai.audio.speech.SpeechResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+
+import java.io.FileOutputStream;
 
 /**
  * @author yunhua
@@ -36,6 +39,8 @@ public class ChatClientController {
     //自动配置类
     @Autowired
     private OpenAiImageModel openAiImageModel;
+    @Autowired
+    private OpenAiAudioSpeechModel openAiAudioSpeechModel ;
 
     @GetMapping("/chat")
     public String generation(@RequestParam(value = "message",defaultValue = "给我讲个笑话") String message) {
@@ -83,6 +88,11 @@ public class ChatClientController {
         return response.getResult().getOutput().getContent() ;
     }
 
+    /**
+     * 文字转图片
+     * @param message
+     * @return
+     */
     @GetMapping("/imageModel")
     public String imageModel(@RequestParam(value = "message",defaultValue = "画一只猫") String message) {
         ImageResponse response = openAiImageModel.call(
@@ -97,6 +107,38 @@ public class ChatClientController {
         );
 
         return response.getResult().getOutput().getUrl() ;
+    }
+
+    /**
+     * 文字转语音
+     * @param message
+     * @return
+     */
+    @GetMapping("/text2Audio")
+    public String text2Audio(@RequestParam(value = "message",defaultValue = "hello,大家好我是王顺") String message) {
+        OpenAiAudioSpeechOptions speechOptions = OpenAiAudioSpeechOptions.builder()
+                .withModel("tts-1")
+                //语音的声音
+                .withVoice(OpenAiAudioApi.SpeechRequest.Voice.ALLOY)
+                .withResponseFormat(OpenAiAudioApi.SpeechRequest.AudioResponseFormat.MP3)
+                .withSpeed(1.0f)
+                .build();
+
+        SpeechPrompt speechPrompt = new SpeechPrompt(message, speechOptions);
+        SpeechResponse response = openAiAudioSpeechModel.call(speechPrompt);
+        byte[] body = response.getResult().getOutput() ;
+        try {
+            writeByteArray2Mp3(body,System.getProperty("user.dir")) ;
+        }catch (Exception e){
+            throw  new RuntimeException(e);
+        }
+        return "ok" ;
+    }
+
+    public static void writeByteArray2Mp3(byte[] body,String dir) throws Exception{
+        FileOutputStream fos = new FileOutputStream(dir+"/wangshun.mp3") ;
+        fos.write(body);
+        fos.close();
     }
 }
 

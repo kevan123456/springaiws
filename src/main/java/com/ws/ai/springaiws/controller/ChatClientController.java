1df796a21324ded21d96395a0d94591ec07904b0
e-shun.wang3 <e-shun.wang3@zeekrlife.com>
2024-12-10 13:09:55 +0800

    charModel

--- src/main/java/com/ws/ai/springaiws/controller/ChatClientController.java
+++ src/main/java/com/ws/ai/springaiws/controller/ChatClientController.java
@@ -2,6 +2,10 @@ package com.ws.ai.springaiws.controller;
 
 import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +25,11 @@ public class ChatClientController {
 
     @Autowired
     private ChatClient chatClient;
+    @Autowired
+    private ChatModel chatModel ;
 
     @GetMapping("/chat")
-    String generation(@RequestParam(value = "message",defaultValue = "给我讲个笑话") String message) {
+    public String generation(@RequestParam(value = "message",defaultValue = "给我讲个笑话") String message) {
         //prompt提示词
         return this.chatClient.prompt()
                 //用户信息
@@ -40,7 +46,7 @@ public class ChatClientController {
      * @return
      */
     @GetMapping(value = "/stream",produces = "text/html;charset=UTF-8")
-    Flux<String> stream(@RequestParam(value = "message",defaultValue = "给我讲个笑话") String message) {
+    public Flux<String> stream(@RequestParam(value = "message",defaultValue = "给我讲个笑话") String message) {
         //prompt提示词
         Flux<String> output = chatClient.prompt()
                 .user(message)
@@ -48,6 +54,24 @@ public class ChatClientController {
                 .stream()
                 .content();
         return output ;
+    }
+
+    /**
+     * chatModel是大模型独有的功能定制化配置。chatClient都是通用的，底层封装chatModel
+     * @param message
+     * @return
+     */
+    @GetMapping("/chatModel")
+    public String chatModel(@RequestParam(value = "message",defaultValue = "给我讲个笑话") String message) {
+        ChatResponse response = chatModel.call(
+                new Prompt(
+                        message,
+                        OpenAiChatOptions.builder()
+                                .withModel("gpt-4-32k")
+                                .withTemperature(0.4)
+                                .build()
+                ));
+        return response.getResult().getOutput().getContent() ;
     }
 }
 

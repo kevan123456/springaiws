e1f4d7384128149d720bd7e93c4f2e7d4aed0b01
e-shun.wang3 <e-shun.wang3@zeekrlife.com>
2024-12-10 12:49:45 +0800

    代理

--- src/main/java/com/ws/ai/springaiws/SpringaiwsApplication.java
+++ src/main/java/com/ws/ai/springaiws/SpringaiwsApplication.java
@@ -7,6 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringaiwsApplication {
 
     public static void main(String[] args) {
+        //如果直接访问openAI，需要选择代理
+        /*
+        String proxyHost = "127.0.0.1";
+        int port = 7890 ;
+        System.setProperty("proxyType","4");
+        System.setProperty("proxyPort",String.valueOf(port));
+        System.setProperty("proxyHost",proxyHost);
+        System.setProperty("proxySet","true");
+        */
         SpringApplication.run(SpringaiwsApplication.class, args);
     }
 
